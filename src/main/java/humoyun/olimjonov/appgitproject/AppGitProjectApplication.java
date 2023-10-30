5d9575700740a8684551af449da5eafdea757325
Humoyun-java <olimjonovhumoyun239@gmail.com>
2023-10-30 15:38:26 +0500

    first commit

---
+++ src/main/java/humoyun/olimjonov/appgitproject/AppGitProjectApplication.java
@@ -0,0 +1,13 @@
+package humoyun.olimjonov.appgitproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppGitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppGitProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/humoyun/olimjonov/appgitproject/AppGitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package humoyun.olimjonov.appgitproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppGitProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
