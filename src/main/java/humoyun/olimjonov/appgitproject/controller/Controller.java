11b34a3f7286d506ddb8ed3b2aadb3213db870d3
Humoyun-java <olimjonovhumoyun239@gmail.com>
2023-10-30 15:48:42 +0500

    end points added

--- src/main/java/humoyun/olimjonov/appgitproject/controller/Controller.java
+++ src/main/java/humoyun/olimjonov/appgitproject/controller/Controller.java
@@ -1,5 +1,32 @@
 package humoyun.olimjonov.appgitproject.controller;
 
-public class Controller {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class Controller {
+    @GetMapping("/first")
+    public String firstMethod(){
+        return "First method";
+    }
+
+    @GetMapping("/second")
+    public String secondMethod(){
+        return "Second method";
+    }
+
+    @GetMapping("/third")
+    public String thirdMethod(){
+        return "Third method";
+    }
+
+    @GetMapping("/forth")
+    public String forthMethod(){
+        return "Forth method";
+    }
+
+    @GetMapping("/fifth")
+    public String fifthMethod(){
+        return "Fifth method";
+    }
 }
